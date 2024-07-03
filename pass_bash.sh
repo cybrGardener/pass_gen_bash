35ccc6c2ad747a46ae5453643951c71ffc269cf2
Cbr_Grdnr <95870761+cybrGardener@users.noreply.github.com>
2024-07-03 14:46:44 -0400

    added different functions for different characters

--- pass_bash.sh
+++ pass_bash.sh
@@ -1,7 +1,40 @@
 #!/usr/bin/env bash
 
-random_pass() {
-    /dev/random > tr -dc 'A-Za-z0-9_' |
-    head -c 10
+random_pass1() {
+   < /dev/random tr -dc [:punct:] |
+    head -c 10;
     echo
 }
+
+random_pass2() {
+   < /dev/random tr -dc [:upper:] |
+   head -c 10;
+   echo
+}
+
+random_pass3() {
+   < /dev/random tr -dc [:digit:] |
+   head -c 10;
+   echo
+}
+
+random_pass4() {
+   < /dev/random tr -dc [:lower:] |
+   head -c 10;
+   echo
+}
+
+random_pass1
+random_pass2
+random_pass3
+random_pass4
+
+var1=$random_pass1
+var2=$random_pass2
+var3=$random_pass3
+var4=$random_pass4
+
+echo "$var1"
+echo "$var2"
+echo "$var3"
+echo "$var4"
